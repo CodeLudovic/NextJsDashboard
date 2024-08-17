67956d0da7f9abbb0e61c65eddb3ed46c2010068
danielor92 <danielospinar@gmail.com>
2024-08-17 17:39:40 -0500

    Interfaces Pokemons

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,19 +1,20 @@
-
-import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 import Image from "next/image";
 
-const getPokemons = async (limit = 20, offset = 0):Promise<SimplePokemon[]> => {
+const getPokemons = async (
+	limit = 20,
+	offset = 0
+): Promise<SimplePokemon[]> => {
 	const data: PokemonsResponse = await fetch(
 		`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
 	).then((response) => response.json());
-	
-	const pokemons = data.results.map(pokemon => ({
+
+	const pokemons = data.results.map((pokemon) => ({
 		id: pokemon.url.split("/").at(-2)!,
-		name: pokemon.name
-	}))
+		name: pokemon.name,
+	}));
 
-	return pokemons
-
+	return pokemons;
 };
 
 export default async function PokemonsPage() {
@@ -21,18 +22,12 @@ export default async function PokemonsPage() {
 
 	return (
 		<div className="flex flex-col">
-			{/* <div className="flex flex-wrap gap-10 items-center justify-center">
-				{pokemons.map((pokemon) => (
-					<Image
-						src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png`}
-						alt={`pokemon 132`}
-						width={100}
-						height={100}
-						key={pokemon.ic}
-					/>
-				))}
-			</div> */}
-			{JSON.stringify(pokemons)}
+			<span className="text-5xl my-2">
+				{" "}
+				Listado de Pokémons <small>estático</small>
+			</span>
+			<PokemonGrid pokemons={pokemons} />
+			{/* {JSON.stringify(pokemons)} */}
 		</div>
 	);
 }
--- app/pokemons/index.ts
+++ app/pokemons/index.ts
@@ -1,2 +1,5 @@
+import { PokemonCard } from './components/PokemonCard';
+export { PokemonGrid } from "./components/PokemonGrid";
+
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
